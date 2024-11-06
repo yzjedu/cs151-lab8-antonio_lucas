b9afb8e25ceb84845165257680ea7101af939349
lapodowski <lapodowski@loyola.edu>
2024-11-06 17:27:06 -0500

    main.py push 1

---
+++ main.py
@@ -0,0 +1,65 @@
+import random
+
+# Purpose: prompting user to input the number of times they want to roll dice
+# name: roll_input
+# parameter: none
+# return: num_rolls
+def roll_input():
+    num_rolls = input("Enter the number of times you want to roll the pair of dice: ")
+    while not num_rolls.isdigit():
+        print('Input must be an integer')
+        num_rolls = input("Enter the number of times you want to roll the pair of dice: ")
+    return int(num_rolls)
+
+# Purpose: roll the die the number of times they want and store in list
+# name: roller
+# parameter: num_rolls
+# return: list_of_values
+def roller(num_rolls):
+    count = 0
+    list_of_values = []
+    while count < num_rolls:
+        roll1 = random.randint(1,6)
+        roll2 = random.randint(1,6)
+        roll_sum = roll1 + roll2
+        list_of_values.append(roll_sum)
+        count += 1
+    return list_of_values
+
+# Purpose:  count the number of times a value is rolled
+# name: times_rolled
+# parameter: list of values
+# return:  count_list
+def times_rolled(list_of_values,num_rolls):
+    sums_list = []
+    count = 2
+    sum_count = 0
+    while count <= 12 :
+        sum_count = list_of_values.count(count)
+        sums_list.append(sum_count)
+        count += 1
+    return sums_list
+
+# Purpose:  output the sum of list with *
+# name: sum_chart
+# parameter: sums_list
+# return:  none
+def sum_chart(sums_list):
+    count = 2
+    sum_count = 0
+    while count < 12:
+        print(f"Sum of {count}", '*' * sums_list[sum_count])
+        count += 1
+        sum_count += 1
+
+# Main function
+def main():
+    num_rolls = roll_input()
+    list_of_values = roller(num_rolls)
+    sums_list = times_rolled(list_of_values,num_rolls)
+
+    print(f"\nRolling {num_rolls} pairs of dice.")
+    print(sums_list)
+    sum_chart(sums_list)
+
+main()
